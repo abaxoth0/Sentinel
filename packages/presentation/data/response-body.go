ec71561eaa4e165d5741e3eae496cf17dddbcb27
Stepan Ananin <xrf848@gmail.com>
2025-03-08 11:40:10 +0300

    encoding/json replace via json-iterator

---
+++ packages/presentation/data/response-body.go
@@ -0,0 +1,19 @@
+package datamodel
+
+type TokenResponseBody struct {
+	Message     string `json:"message"`
+	AccessToken string `json:"accessToken"`
+}
+
+type MessageResponseBody struct {
+	Message string `json:"message"`
+}
+
+type LoginExistanceResponseBody struct {
+	Exists bool `json:"exists"`
+}
+
+type UserRolesResponseBody struct {
+	Roles []string `json:"roles"`
+}
+
