85343eba56c18df682721770a5429684e328e059
Stepan Ananin <xrf848@gmail.com>
2025-03-08 11:41:07 +0300

    tokens refactoring

--- packages/infrastructure/token/errors.go
+++ packages/infrastructure/token/errors.go
@@ -11,22 +11,22 @@ var accessTokenExpired = Error.NewStatusError(
     http.StatusUnauthorized,
 )
 
-var invalidAccessToken =Error.NewStatusError(
+var invalidAccessToken = Error.NewStatusError(
     "Invalid Access Token",
     http.StatusBadRequest,
 )
 
-var unauthorized =Error.NewStatusError(
+var unauthorized = Error.NewStatusError(
     "Вы не авторизованы",
     http.StatusUnauthorized,
 )
 
-var invalidRefreshToken =Error.NewStatusError(
+var invalidRefreshToken = Error.NewStatusError(
     "Invalid Refresh Token",
     http.StatusBadRequest,
 )
 
-var refreshTokenExpired =Error.NewStatusError(
+var refreshTokenExpired = Error.NewStatusError(
     "Refresh Token Expired",
     // Not sure that status 409 is OK for this case,
     // currently this tells user that there are conflict with server and him,
