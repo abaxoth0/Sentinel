71e0cfd19f376a085d8b24694318861ec28aa5b9
Stepan Ananin <xrf848@gmail.com>
2025-03-12 16:38:39 +0300

    added user controller

--- packages/presentation/api/http/router/routes.go
+++ packages/presentation/api/http/router/routes.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 	"sentinel/packages/infrastructure/config"
 	Auth "sentinel/packages/presentation/api/http/controllers/auth"
+	User "sentinel/packages/presentation/api/http/controllers/user"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// i could just explicitly pass empty string in routes when i need it
+// but it looks really awful, shitty and not obvious
 const groupRootPath = ""
 
 func Create() *echo.Echo {
@@ -19,6 +22,7 @@ func Create() *echo.Echo {
 
     router.HTTPErrorHandler = handleHttpError
     router.JSONSerializer = serializer{}
+    router.Binder = &binder{}
 
     cors := middleware.CORSConfig{
         Skipper:      middleware.DefaultSkipper,
@@ -48,6 +52,17 @@ func Create() *echo.Echo {
     authGroup.POST(groupRootPath, Auth.Login)
     authGroup.PUT(groupRootPath, Auth.Refresh)
     authGroup.DELETE(groupRootPath, Auth.Logout)
+
+    userGroup := router.Group("/user")
+
+    userGroup.POST(groupRootPath, User.Create)
+    userGroup.DELETE(groupRootPath, User.SoftDelete)
+    userGroup.POST("/restore", User.Restore)
+    userGroup.DELETE("/drop", User.Drop)
+
+    userGroup.PATCH("/login", User.ChangeLogin)
+    userGroup.PATCH("/password", User.ChangePassword)
+    userGroup.PATCH("/roles", User.ChangeRoles)
 
     return router
 }
