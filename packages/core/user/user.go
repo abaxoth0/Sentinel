3812efc48cc409a96d2d78f64d3f4440c8e8bdcb
Stepan Ananin <xrf848@gmail.com>
2025-04-10 22:07:56 +0300

    breaking change: reworked user password and login verification

--- packages/core/user/user.go
+++ packages/core/user/user.go
@@ -2,10 +2,11 @@ package user
 
 import (
 	"net/http"
-	"regexp"
 	"sentinel/packages/common/config"
 	Error "sentinel/packages/common/errors"
+	"sentinel/packages/common/validation"
 	"sentinel/packages/core"
+	"strings"
 )
 
 type Property core.EntityProperty
@@ -35,7 +36,7 @@ var invalidPasswordLength = Error.NewStatusError(
 )
 
 func VerifyPassword(password string) *Error.Status {
-	passwordSize := len(password)
+	passwordSize := len(strings.ReplaceAll(password, " ", ""))
 
 	// bcrypt can handle password with maximum size of 72 bytes
 	if passwordSize < 8 || passwordSize > 64 {
@@ -45,32 +46,27 @@ func VerifyPassword(password string) *Error.Status {
 	return nil
 }
 
-var invalidLoginLength = Error.NewStatusError(
-    "Логин должен находиться в диапозоне от 5 до 72 символов.",
-    http.StatusBadRequest,
-)
-
-var invalidEmailFormat = Error.NewStatusError(
-    "Неверный логин: неподустимый формат E-Mail'а",
-    http.StatusBadRequest,
-)
-
-// Pretty close to RFC 5322 solution,
-// but it's still not providing full features (like comments)
-// and most likely will not handle all edge cases perfectly.
-// But in this case, that's enough.
-var emailPattern = regexp.MustCompile(`(?i)^(?:[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\x60{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$`)
-
 func VerifyLogin(login string) *Error.Status {
-    if length := len(login); length < 5 || length > 72 {
-        return invalidLoginLength
+    length := len(strings.ReplaceAll(login, " ", ""))
+
+    if length < 5 || length > 72 {
+        return Error.NewStatusError(
+            "Логин должен находиться в диапозоне от 5 до 72 символов.",
+            http.StatusBadRequest,
+        )
     }
 
     if config.App.IsLoginEmail {
-        if !emailPattern.MatchString(login) {
-            return invalidEmailFormat
+        if err := validation.Email(login); err != nil {
+            // If err is not nil then it maybe only Error.InvalidValie,
+            // cuz login was already checked for zero or whitespaces value
+            return Error.NewStatusError(
+                "Неверный логин: неподустимый формат E-Mail'а",
+                http.StatusBadRequest,
+            )
         }
     }
+
     return nil
 }
 
