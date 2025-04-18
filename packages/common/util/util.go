a84ca263392f8d386bd732c8b2189e4291a93684
Stepan Ananin <xrf848@gmail.com>
2025-04-18 12:17:48 +0300

    removed unused utils

--- packages/common/util/util.go
+++ packages/common/util/util.go
@@ -1,17 +1,5 @@
 package util
 
-import (
-	"time"
-)
-
-func TimestampSinceNow(t time.Duration) int64 {
-	return time.Now().Add(t).UTC().UnixMilli()
-}
-
-func UnixTimeNow() int64 {
-	return time.Now().UTC().UnixMilli()
-}
-
 // Ternary operator.
 // If 'cond' is true then returns 'a', otherwise returns 'b'
 func Ternary[T any](cond bool, a T, b T) T {
