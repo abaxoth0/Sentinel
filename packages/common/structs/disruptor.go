47638624d4b68f40c7ca0e7e9437c6197435b61d
Stepan Ananin <xrf848@gmail.com>
2025-04-30 23:44:17 +0300

    added disruptor struct
    it implements LMAX Disruptor pattern:
    https://lmax-exchange.github.io/disruptor
    
    (it will be used for logs processing)

---
+++ packages/common/structs/disruptor.go
@@ -0,0 +1,108 @@
+package structs
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+const (
+	BufferSize      = 1 << 16 // Power of 2
+	BufferIndexMask = BufferSize - 1
+)
+
+var yieldWaiter = yieldWait{}
+
+type sequence struct {
+    Value atomic.Int64
+    // Padding to prevent false sharing
+    _padding [56]byte
+}
+
+type waiter interface {
+    WaitFor(seq int64, cursor *sequence, done <-chan struct{})
+}
+
+type yieldWait struct{}
+
+func (w yieldWait) WaitFor(seq int64, cursor *sequence, done <-chan struct{}) {
+    for cursor.Value.Load() < seq {
+        select {
+        case <-done:
+            return
+        default:
+            time.Sleep(10 * time.Microsecond)
+        }
+    }
+}
+
+type Disruptor[T any] struct {
+	buffer  [BufferSize]T
+	writer  sequence // write position (starts at -1)
+	reader  sequence // read position (starts at -1)
+	waiter  waiter
+	done    chan struct{}
+}
+
+func NewDisruptor[T any]() *Disruptor[T] {
+	d := &Disruptor[T]{
+		done:   make(chan struct{}),
+		waiter: yieldWaiter,
+	}
+	d.writer.Value.Store(-1)
+	d.reader.Value.Store(-1)
+	return d
+}
+
+func (d *Disruptor[T]) Close() {
+	close(d.done)
+}
+
+func (d *Disruptor[T]) Publish(entry T) bool {
+    select {
+    case <-d.done:
+        return false
+    default:
+        writer := d.writer.Value.Load()
+        reader := d.reader.Value.Load()
+        nextWriter := writer + 1
+
+        // Check if buffer is full
+        // NOTE: For buffer sizes ≤ 8, use (nextWriter - reader) > (BufferSize - 1)
+        // to avoid off-by-one overwrites. For larger buffers (≥1024), the current check
+        // is sufficient and more performant.
+        if nextWriter - reader >= BufferSize {
+            return false
+        }
+
+        d.buffer[nextWriter&BufferIndexMask] = entry
+        d.writer.Value.Store(nextWriter)
+        return true
+    }
+}
+
+func (d *Disruptor[T]) Consume(handler func(T)) {
+	var claimed int64 = d.reader.Value.Load() + 1
+
+	for {
+		select {
+		case <-d.done:
+			return
+		default:
+			writer := d.writer.Value.Load()
+
+			if claimed > writer {
+				d.waiter.WaitFor(claimed, &d.writer, d.done)
+				continue
+			}
+
+			// Process all entries from claimed to current writer
+			for i := claimed; i <= writer; i++ {
+				entry := d.buffer[i&BufferIndexMask]
+				handler(entry)
+				d.reader.Value.Store(i)
+			}
+			claimed = writer + 1 // Move to next unprocessed entry
+		}
+	}
+}
+
