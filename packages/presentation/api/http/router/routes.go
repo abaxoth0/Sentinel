69b4eb14217b3d6f809d4c2921ccc0b99a907f92
Stepan Ananin <xrf848@gmail.com>
2025-08-14 22:53:35 +0300

    make activation token resend endpoint unsecure

--- packages/presentation/api/http/router/routes.go
+++ packages/presentation/api/http/router/routes.go
@@ -120,7 +120,7 @@ func Create() *echo.Echo {
     userGroup.PATCH("/:uid/password", User.ChangePassword, secure, preventUserDesync, doubleSubmitCSRF)
     userGroup.PATCH("/:uid/roles", User.ChangeRoles, secure, preventUserDesync, doubleSubmitCSRF)
     userGroup.GET("/activation/:token", Activation.Activate)
-    userGroup.PUT("/activation/resend", Activation.Resend, secure, preventUserDesync)
+    userGroup.PUT("/activation/resend", Activation.Resend)
 	userGroup.GET("/search", User.SearchUsers, secure, preventUserDesync)
 	userGroup.GET("/:uid/sessions", User.GetUserSessions, secure, preventUserDesync)
 	userGroup.GET("/:uid", User.GetUser, secure, preventUserDesync)
